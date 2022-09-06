0c719099af7b6062543f1ffabda424a86c3f8986
msakuta <masahiro.sakuta@gmail.com>
2022-09-07 00:58:35 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+    fn main() {
+    let mut stack = vec![];
+
+    stack.push(42);
+    stack.push(36);
+
+    add(&mut stack);
+
+    stack.push(22);
+
+    add(&mut stack);
+
+    println!("stack: {stack:?}");
+}
+
+fn add(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+    stack.push(lhs + rhs);
+}
