25daa4074ff53100ccdc988466da32bf2e4e28da
msakuta <masahiro.sakuta@gmail.com>
2022-09-07 01:26:53 +0900

    Add read example

---
+++ examples/01-read.rs
@@ -0,0 +1,27 @@
+fn main() {
+    for line in std::io::stdin().lines() {
+        let mut stack = vec![];
+        if let Ok(line) = line {
+            let words: Vec<_> = line.split(" ").collect();
+
+            for word in words {
+                if let Ok(parsed) = word.parse::<i32>() {
+                    stack.push(parsed);
+                } else {
+                    match word {
+                        "+" => add(&mut stack),
+                        _ => panic!("{word:?} could not be parsed"),
+                    }
+                }
+            }
+
+            println!("stack: {stack:?}");
+        }
+    }
+}
+
+fn add(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+    stack.push(lhs + rhs);
+}
